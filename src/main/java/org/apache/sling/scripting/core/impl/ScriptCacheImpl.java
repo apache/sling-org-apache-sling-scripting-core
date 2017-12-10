b68a89f46629c6d661d65ec9f0fc9067cccc24fe
Oliver Lietz <olli@apache.org>
2017-12-10 12:56:29 +0100

    remove unused imports

--- src/main/java/org/apache/sling/scripting/core/impl/ScriptCacheImpl.java
+++ src/main/java/org/apache/sling/scripting/core/impl/ScriptCacheImpl.java
@@ -56,8 +56,6 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
