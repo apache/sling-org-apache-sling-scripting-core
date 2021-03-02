faae160193e3303b7e10b345ba68f225cf8891d2
Radu Cotescu <radu@apache.org>
2021-03-02 14:20:14 +0100

    SLING-9999 - Remove cyclic dependency between scripting and servlets features
    * moved the o.a.s.servlets.resolver.bundle.tracker API to a dedicated bundle

--- src/main/java/org/apache/sling/scripting/core/impl/bundled/AbstractBundledRenderUnit.java
+++ src/main/java/org/apache/sling/scripting/core/impl/bundled/AbstractBundledRenderUnit.java
@@ -36,7 +36,7 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.scripting.core.impl.helper.OnDemandReaderRequest;
 import org.apache.sling.scripting.core.impl.helper.OnDemandWriterResponse;
-import org.apache.sling.servlets.resolver.bundle.tracker.TypeProvider;
+import org.apache.sling.scripting.spi.bundle.TypeProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.framework.Bundle;
