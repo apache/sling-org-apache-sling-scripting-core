fea92c5884669c2998fc07ee9047863f665f0858
David Bosschaert <bosschae@adobe.com>
2022-02-04 10:34:38 +0000

    Update test as requested by @raducotescu

--- src/test/java/org/apache/sling/scripting/core/impl/bundled/AbstractBundledRenderUnitTest.java
+++ src/test/java/org/apache/sling/scripting/core/impl/bundled/AbstractBundledRenderUnitTest.java
@@ -18,14 +18,9 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package org.apache.sling.scripting.core.impl.bundled;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Field;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
@@ -66,22 +61,21 @@ public class AbstractBundledRenderUnitTest {
                 return "test";
             }};
 
-        Field rl = AbstractBundledRenderUnit.class.getDeclaredField("references");
-        rl.setAccessible(true);
-        assertNull("Precondition", rl.get(abru));
-
-        Field sf = AbstractBundledRenderUnit.class.getDeclaredField("services");
-        sf.setAccessible(true);
-        assertNull("Precondition", sf.get(abru));
-
         assertSame(fooBarSvc, abru.getService("foo.Bar"));
 
-        List references = (List) rl.get(abru);
-        assertTrue(references.contains(sref));
-        Map servicesMap = (Map) sf.get(abru);
-        assertSame(fooBarSvc, servicesMap.get("foo.Bar"));
+        // Make the service registry return a new service now
+        Object newSvc = new Object();
+        ServiceReference sref2 = Mockito.mock(ServiceReference.class);
+        Mockito.when(bc.getServiceReference("foo.Bar"))
+            .thenReturn(sref2);
+        Mockito.when(bc.getService(sref2))
+            .thenReturn(newSvc);
 
-        assertSame("Second invocation should return the service from the cache",
+        assertSame("Second invocation should return the old service from the cache",
                 fooBarSvc, abru.getService("foo.Bar"));
+
+        abru.releaseDependencies();
+        assertSame("After cleaning the cache check that the new service is returned",
+                newSvc, abru.getService("foo.Bar"));
     }
 }
