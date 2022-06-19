aa00441837a42c111e9d61d487ac614682df8ac0
Eric Norman <enorman@apache.org>
2022-06-18 18:30:51 -0700

    SLING-11398 adjust test for better code coverage
    make sure the groovy bundle starts before scripting-core

--- src/test/java/org/apache/sling/scripting/core/it/SLING_11398IT.java
+++ src/test/java/org/apache/sling/scripting/core/it/SLING_11398IT.java
@@ -19,8 +19,10 @@
 package org.apache.sling.scripting.core.it;
 
 import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.vmOption;
 
 import javax.inject.Inject;
 import javax.script.ScriptEngine;
@@ -31,6 +33,8 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.ModifiableCompositeOption;
+import org.ops4j.pax.exam.options.extra.VMOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
@@ -48,10 +52,24 @@ public class SLING_11398IT extends ScriptingCoreTestSupport {
     public Option[] configuration() {
         return options(
             baseConfiguration(),
-            mavenBundle().groupId("org.codehaus.groovy").artifactId("groovy").version("3.0.9"),
+            optionalRemoteDebug(),
+            mavenBundle().groupId("org.codehaus.groovy").artifactId("groovy").version("3.0.9").startLevel(1),
             // factory is defined in this fragment artifact
             mavenBundle().groupId("org.codehaus.groovy").artifactId("groovy-jsr223").version("3.0.9").noStart()
         );
+    }
+
+    /**
+     * Optionally configure remote debugging on the port supplied by the "debugPort"
+     * system property.
+     */
+    protected ModifiableCompositeOption optionalRemoteDebug() {
+        VMOption option = null;
+        String property = System.getProperty("debugPort");
+        if (property != null) {
+            option = vmOption(String.format("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%s", property));
+        }
+        return composite(option);
     }
 
     @Test
